8f15a0bd0bbe0e00126193a42361c78e8b8a3836
PaulaEs <paulaproxy@gmail.com>
2013-11-01 00:46:09 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MongoidDeviseRails::Application.config.secret_key_base = '16255f68eb277d3e13924f34c18282fa1b8fe2a2b81ccdbb8ee823cf4cd7ea1926b267d24180b6eead2c26e98ed4375e801ddb0f0f93590ad28642b276ce9297'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MongoidDeviseRails::Application.config.session_store :cookie_store, key: '_mongoid-devise-rails_session'
