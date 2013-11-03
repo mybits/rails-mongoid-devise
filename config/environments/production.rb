031e17e154a79d701a2c2ea4b76aeb3984ada420
PaulaEs <paulaproxy@gmail.com>
2013-11-03 02:21:39 +0100

    Configure ActionMailer.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,12 @@ MongoidDeviseRails::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { host: 'yourhost.com'}
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default charset: 'utf-8'
 end
