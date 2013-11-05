881cf082d143cd005cbf6e8c19028cb346a6d9e1
PaulaEs <paulaproxy@gmail.com>
2013-11-05 01:27:52 +0100

    Modyfing user model. Generating and customizing Devise views.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User
   include Mongoid::Document
+
+  field :name, type: String
+  validates_presence_of :name
+  validates_uniqueness_of :name, :email, :case_sensitive => false
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
