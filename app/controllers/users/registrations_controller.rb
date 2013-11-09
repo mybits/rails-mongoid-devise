b7c703284ad3d49e38f97502f2aba43139a27df5
PaulaEs <paulaproxy@gmail.com>
2013-11-09 20:27:00 +0100

    Devise sanitized params.

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,10 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :update_sanitized_params, if: :devise_controller?
+
+  protected
+
+  def update_sanitized_params
+    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation)
+    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:name, :email, :current_password)}
+  end
+end
